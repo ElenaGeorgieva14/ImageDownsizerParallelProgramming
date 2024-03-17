0990ad71c5672ac5bb41e9fc9e2da623b0950bec
Elena Georgieva <elenageorgieva44@gmail.com>
2024-03-17 10:05:02 +0200

    Initial Commit

--- Image Downsizer/Form1.Designer.cs
+++ Image Downsizer/Form1.Designer.cs
@@ -31,15 +31,21 @@
             this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
             this.fileSystemWatcher2 = new System.IO.FileSystemWatcher();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.UploadImageBtn = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.button2 = new System.Windows.Forms.Button();
             this.downscalingFactorTB = new System.Windows.Forms.NumericUpDown();
             this.button3 = new System.Windows.Forms.Button();
+            this.ConsTime = new System.Windows.Forms.Label();
+            this.resizedImagePictureBox = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.ParallelTimeL = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.downscalingFactorTB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resizedImagePictureBox)).BeginInit();
             this.SuspendLayout();
             // 
             // fileSystemWatcher1
@@ -55,28 +61,28 @@
             // pictureBox1
             // 
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(9, 37);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(600, 293);
+            this.pictureBox1.Size = new System.Drawing.Size(498, 293);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
-            // button1
+            // UploadImageBtn
             // 
-            this.button1.Location = new System.Drawing.Point(9, 314);
-            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(99, 32);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "Upload Image";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.UploadImageBtn.Location = new System.Drawing.Point(11, 334);
+            this.UploadImageBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.UploadImageBtn.Name = "UploadImageBtn";
+            this.UploadImageBtn.Size = new System.Drawing.Size(99, 32);
+            this.UploadImageBtn.TabIndex = 1;
+            this.UploadImageBtn.Text = "Upload Image";
+            this.UploadImageBtn.UseVisualStyleBackColor = true;
+            this.UploadImageBtn.Click += new System.EventHandler(this.UploadImageBtn_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(122, 319);
+            this.label1.Location = new System.Drawing.Point(126, 340);
             this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(98, 13);
@@ -85,53 +91,101 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(337, 319);
-            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Location = new System.Drawing.Point(334, 334);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(56, 19);
             this.button2.TabIndex = 4;
             this.button2.Text = "Resize";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.Resize_Click);
             // 
             // downscalingFactorTB
             // 
-            this.downscalingFactorTB.Location = new System.Drawing.Point(224, 320);
-            this.downscalingFactorTB.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.downscalingFactorTB.Location = new System.Drawing.Point(240, 335);
+            this.downscalingFactorTB.Margin = new System.Windows.Forms.Padding(2);
             this.downscalingFactorTB.Name = "downscalingFactorTB";
             this.downscalingFactorTB.Size = new System.Drawing.Size(90, 20);
             this.downscalingFactorTB.TabIndex = 5;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(425, 320);
+            this.button3.Location = new System.Drawing.Point(410, 334);
             this.button3.Margin = new System.Windows.Forms.Padding(2);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(97, 19);
             this.button3.TabIndex = 6;
-            this.button3.Text = "Resize Parallels";
+            this.button3.Text = "Resize Parallel";
             this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.Click += new System.EventHandler(this.resizeParallelBTN_Click);
+            // 
+            // ConsTime
+            // 
+            this.ConsTime.AutoSize = true;
+            this.ConsTime.Location = new System.Drawing.Point(331, 365);
+            this.ConsTime.Name = "ConsTime";
+            this.ConsTime.Size = new System.Drawing.Size(0, 13);
+            this.ConsTime.TabIndex = 7;
+            // 
+            // resizedImagePictureBox
+            // 
+            this.resizedImagePictureBox.Location = new System.Drawing.Point(555, 37);
+            this.resizedImagePictureBox.Name = "resizedImagePictureBox";
+            this.resizedImagePictureBox.Size = new System.Drawing.Size(473, 293);
+            this.resizedImagePictureBox.TabIndex = 8;
+            this.resizedImagePictureBox.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(570, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Resized Image";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Original Image";
+            // 
+            // ParallelTimeL
+            // 
+            this.ParallelTimeL.AllowDrop = true;
+            this.ParallelTimeL.AutoSize = true;
+            this.ParallelTimeL.Location = new System.Drawing.Point(331, 396);
+            this.ParallelTimeL.Name = "ParallelTimeL";
+            this.ParallelTimeL.Size = new System.Drawing.Size(0, 13);
+            this.ParallelTimeL.TabIndex = 11;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(1068, 418);
+            this.Controls.Add(this.ParallelTimeL);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.resizedImagePictureBox);
+            this.Controls.Add(this.ConsTime);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.downscalingFactorTB);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.UploadImageBtn);
             this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form1";
             this.Text = "Image Downsizer";
-            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.downscalingFactorTB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.resizedImagePictureBox)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -141,12 +195,17 @@
 
         private System.IO.FileSystemWatcher fileSystemWatcher1;
         private System.IO.FileSystemWatcher fileSystemWatcher2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button UploadImageBtn;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.NumericUpDown downscalingFactorTB;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label ConsTime;
+        private System.Windows.Forms.PictureBox resizedImagePictureBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label ParallelTimeL;
     }
 }
 
--- Image Downsizer/Form1.cs
+++ Image Downsizer/Form1.cs
@@ -12,6 +12,7 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Image_Downsizer
 {
@@ -22,53 +23,32 @@ namespace Image_Downsizer
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
         Bitmap originalImageBitmap;
+        Bitmap downsizedImageBitmap;
+        BitmapData originalImageBitmapData;
+        BitmapData downsizedImageBitmapData;
         int originalWidth;
         int originalHeight;
+        int bytesPerPixel;
         byte[] row;
-        private void button1_Click(object sender, EventArgs e)
+        private void UploadImageBtn_Click(object sender, EventArgs e)
         {
             try
             {
                 OpenFileDialog fileDialog = new OpenFileDialog();
                 if (fileDialog.ShowDialog() == DialogResult.OK)
                 {
+                    originalImageBitmap = new Bitmap(fileDialog.FileName);
                     pictureBox1.ImageLocation = fileDialog.FileName;
-                    string path = Path.Combine(@"~\image");
-
-                    if (!Directory.Exists(path))
-                    {
-                        Directory.CreateDirectory(path);
-                    }
-
-                    var fileName = Path.GetFileName(fileDialog.FileName);
-                    Console.WriteLine("fileName: " + fileName);
-                    path = path + fileName;
-                    Console.WriteLine("path: " + path);
-                    File.Copy(fileDialog.FileName, path, true);
-
-                    originalImageBitmap = new Bitmap(path);
-               
                     originalWidth = originalImageBitmap.Width;
                     originalHeight = originalImageBitmap.Height;
-
-                    Rectangle originalImageRect = new Rectangle(0, 0, originalImageBitmap.Width, originalImageBitmap.Height);
-
-                    BitmapData originalImageBitmapData = originalImageBitmap.LockBits(originalImageRect, ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
-
-                    originalImageBitmap.UnlockBits(originalImageBitmapData);
+                    originalImageBitmapData = originalImageBitmap.LockBits(new Rectangle(0, 0, originalWidth, originalHeight), ImageLockMode.ReadOnly, originalImageBitmap.PixelFormat);
 
                     IntPtr ptr = originalImageBitmapData.Scan0;
                     int bytes = originalImageBitmapData.Stride * originalImageBitmapData.Height;
-                    row = new byte[bytes ];
+                    row = new byte[bytes];
 
                     Marshal.Copy(ptr, row, 0, bytes);
-
-                   
                 }
             }
             catch (Exception ex)
@@ -79,76 +59,101 @@ namespace Image_Downsizer
 
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void Resize_Click(object sender, EventArgs e)
         {
+            Stopwatch consequenceResizeTime = new Stopwatch();
+            consequenceResizeTime.Start();
             double downscalingFactor =  getDownscalingFactor();
             int newWidth = (int)(originalWidth * downscalingFactor);
             int newHeight = (int)(originalHeight * downscalingFactor);
             byte[] downsizedImageData = new byte[(newWidth * newHeight) * 3];
-            
-            // Nearest Neighbor Downsizing
-            for (int y = 0; y < newHeight; y++)
-            {
-                calculateNewPixelData(newWidth, downscalingFactor, downsizedImageData, y);
-            }
+            downsizedImageBitmap = new Bitmap(newWidth, newHeight, originalImageBitmap.PixelFormat);
+            downsizedImageBitmapData = downsizedImageBitmap.LockBits(new Rectangle(0, 0, newWidth, newHeight), ImageLockMode.WriteOnly, originalImageBitmap.PixelFormat);
 
+            // Nearest Neighbor Downsizing
+            calculateNewPixelData(newWidth, downscalingFactor, downsizedImageData, 0,newHeight);
+          
             String pictureName = "downsized_image.jpg";
 
-            savePicture(newWidth, newHeight, downsizedImageData, pictureName);
+            savePicture(downsizedImageData, pictureName);
+            ConsTime.Text = "Resizing time with consequence algorithm is: " + consequenceResizeTime.Elapsed.ToString();
+            consequenceResizeTime.Reset();
+
         }
 
-        private void button3_Click(object sender, EventArgs e)
+        private void resizeParallelBTN_Click(object sender, EventArgs e)
         {
+            Stopwatch parallelResizeTime = new Stopwatch();
+            parallelResizeTime.Start();
             double downscalingFactor = getDownscalingFactor();
             int newWidth = (int)(originalWidth * downscalingFactor);
             int newHeight = (int)(originalHeight * downscalingFactor);
             byte[] downsizedImageData = new byte[(newWidth * newHeight) * 3];
+            downsizedImageBitmap = new Bitmap(newWidth, newHeight, originalImageBitmap.PixelFormat);
+            downsizedImageBitmapData = downsizedImageBitmap.LockBits(new Rectangle(0, 0, newWidth, newHeight), ImageLockMode.WriteOnly, originalImageBitmap.PixelFormat);
 
+            int numThreads = Environment.ProcessorCount;
+            int rowsPerThread = newHeight / numThreads;
+
+
+            List<Thread> threads = new List<Thread>();
             // Nearest Neighbor Downsizing
-            for (int y = 0; y < newHeight; y++)
+
+            for (int i = 0; i < numThreads; i++)
             {
-                Thread thread = new Thread(() => calculateNewPixelData(newWidth, downscalingFactor, downsizedImageData, y));
+                int startY = i * rowsPerThread;
+                int endY = (i == numThreads - 1) ? newHeight : (i + 1) * rowsPerThread;
+            
+                Thread thread = new Thread(() => calculateNewPixelData(newWidth, downscalingFactor, downsizedImageData, startY,endY));
                 
+                threads.Add(thread);
                 thread.Start();
-                thread.Join();
             }
 
+            foreach (Thread thread in threads)
+            {
+                thread.Join();
+            }
             String pictureName = "downsized_image_parallel.jpg";
 
-            savePicture(newWidth, newHeight, downsizedImageData, pictureName);
+            savePicture(downsizedImageData, pictureName);
+
+            ParallelTimeL.Text =  "Resizing time with parallel algorithm is: " + parallelResizeTime.Elapsed.ToString();
+            parallelResizeTime.Reset();
         }
 
         private double getDownscalingFactor()
         {
-            int selectedPercentage = 100 - (int)downscalingFactorTB.Value;
-            double downscalingFactor = selectedPercentage / 100.0;
-
-            return downscalingFactor;
+            return (double)downscalingFactorTB.Value / 100.0;
         }
 
-        private void calculateNewPixelData(int newWidth, double downscalingFactor, byte[] downsizedImageData, int y)
+        private void calculateNewPixelData(int newWidth, double downscalingFactor, byte[] downsizedImageData, int startY,int endY)
         {
-            for (int x = 0; x < newWidth; x++)
+            unsafe
             {
-                int originalX = (int)(x * downscalingFactor);
-                int originalY = (int)(y * downscalingFactor);
+                    for (int y = startY; y < endY; y++)
+                    {
+                        for (int x = 0; x < newWidth; x++)
+                        {
+                            int originalX = (int)(x / downscalingFactor);
+                            int originalY = (int)(y / downscalingFactor);
 
-                int originalIndex = (originalY * originalWidth + originalX) * 3;
-                int downsizedIndex = (y * newWidth + x) * 3;
-
-                downsizedImageData[downsizedIndex] = row[originalIndex];
-                downsizedImageData[downsizedIndex + 1] = row[originalIndex + 1];
-                downsizedImageData[downsizedIndex + 2] = row[originalIndex + 2];
+                            int originalIndex = (originalY * originalWidth + originalX) * 3;
+                            int downsizedIndex = (y * newWidth + x) * 3;
+                            downsizedImageData[downsizedIndex] = row[originalIndex];
+                            downsizedImageData[downsizedIndex + 1] = row[originalIndex + 1];
+                            downsizedImageData[downsizedIndex + 2] = row[originalIndex + 2];
+                        }
+                    }
             }
         }
 
-        private void savePicture(int newWidth, int newHeight, byte[] downsizedImageData, String pictureName)
-        {
-            Bitmap downsizedImageBitmap = new Bitmap(newWidth, newHeight, PixelFormat.Format24bppRgb);
-            BitmapData downsizedImageBitmapData = downsizedImageBitmap.LockBits(new Rectangle(0, 0, newWidth, newHeight), ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb);
+        private void savePicture(byte[] downsizedImageData, String pictureName)
+        {          
             Marshal.Copy(downsizedImageData, 0, downsizedImageBitmapData.Scan0, downsizedImageData.Length);
             downsizedImageBitmap.UnlockBits(downsizedImageBitmapData);
             downsizedImageBitmap.Save(pictureName);
+            resizedImagePictureBox.ImageLocation = pictureName;
         }
 
     }
